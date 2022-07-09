087c0cb8d7269a9f705efcd3fb30c2aa36d00067
filmons <filmonseare@gmail.com>
2022-07-09 13:25:52 +0200

    Tableau en js

---
+++ cours/Tableau.js
@@ -0,0 +1,16 @@
+let monTableau1 = new Array('un', 'deux', 'trois');
+let monTableau2 = Array('un', 'deux', 'trois');
+let monTableau3 = ['un', 'deux', 'trois'];
+
+// Ancienne version
+let monTableau2DA = new Array(
+    Array('Mark', 'Jeff', 'Bill'),
+    Array('Zuckerberg', 'Bezos', 'Gates')  
+);
+
+// Nouvelle version
+let monTableau2DN = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+console.log(monTableau2DN)
