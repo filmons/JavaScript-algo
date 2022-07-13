30118263b27da0b948d43cc0fdb6de9fe07d4efd
filmons <filmonseare@gmail.com>
2022-07-14 00:06:57 +0200

    La boucle (for...of and for.. in) avec JavaScript and function arrow

--- cours/boucle.js
+++ cours/boucle.js
@@ -11,3 +11,39 @@ for (const fruit of panier2) {
   // console.log(fruit);
   console.log(panier2.indexOf(fruit));
 }
+
+
+// forEach
+let listeDePays = ['France', 'Belgique', 'Japon', 'Maroc'];
+
+// for (const pays of listeDePays) {
+//     console.log(pays);
+// }
+
+// listeDePays.forEach(function(pays){
+//     console.log(pays);
+// });
+
+listeDePays.forEach(pays => console.log(pays));
+
+
+///////
+let monTableauAssociatif = {
+    'prenom'    : 'Mark',
+    'nom'       : 'Zuckerberg',
+    'poste'     : 'PDG de Facebook'
+};
+
+function concatener(tableau) {
+    let chaine = '';
+
+    for (const valeur in tableau) {
+        
+        chaine += (valeur + ' : ' + tableau[valeur] + '\n');
+
+    }
+
+    console.log(chaine);
+}
+
+concatener(monTableauAssociatif);
