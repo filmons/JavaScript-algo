a8480bdcbda8d3202bed36ed52545c8598081e16
filmons <filmonseare@gmail.com>
2022-07-15 22:57:59 +0200

    function =>  et [200~CodePen Home

---
+++ cours/functions.js
@@ -0,0 +1,51 @@
+let maFonction1 = function(){
+    console.log('test');
+  }
+  
+  let maFonction2 = () => {
+    console.log('test');
+  }
+  
+  let maFonction3 = () => console.log('test');
+  
+  // Dans la pratique, on l'utilise surtout pour les petites fonctions anonymes
+  // Nous verrons plus tard que ces fonctions fléchées n'ont pas de this
+
+
+  // function bonjour(prenom) {
+  
+//   let resultat = "Bonjour " + prenom; // Variable locale
+//   let maClosure = () => console.log(resultat);
+//   return maClosure;
+  
+// }
+
+// function bonjour_bis(prenom) {
+  
+//   let resultat = "Bonjour " + prenom; // Variable locale
+//   console.log(resultat);
+  
+// }
+
+// let maFonction = bonjour('Evan');
+// maFonction();
+// bonjour_bis('Nicolas');
+
+function timer() {
+    let secondes = 0;
+    
+    let maClosure = () => {
+      return ++secondes;
+    }
+    return maClosure;
+  }
+  
+  let monTimer = timer();
+  console.log(monTimer());
+  console.log(monTimer());
+  console.log(monTimer());
+  
+  let monDeuxiemeTimer = timer();
+  console.log(monDeuxiemeTimer());
+  
+  console.log(monTimer());
