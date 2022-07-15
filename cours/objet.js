50c91254b4f85efacbb98ef80cec81f34bfc2a6f
filmons <filmonseare@gmail.com>
2022-07-15 23:18:24 +0200

    Sans décomposition et avec  décomposition

--- cours/objet.js
+++ cours/objet.js
@@ -23,3 +23,18 @@ chien.aboyer();
 
 // magicien.attaquer();
 // guerrier.attaquer();
+/////////////////////////////////////////////
+
+let informations = ['superSayen', '25', 'homme'];
+
+// Sans décomposition
+// let pseudo = informations[0];
+// let age    = informations[1];
+// let sexe   = informations[2];
+
+// Avec décomposition
+let [pseudo, age, sexe] = informations;
+
+console.log(pseudo);
+console.log(age);
+console.log(sexe);
